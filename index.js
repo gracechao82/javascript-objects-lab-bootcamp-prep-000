5ef7df3c976ee3103336ac56b065ec2583bc0153
gracey12051@gmail.com <gracey12051@gmail.com>
2018-04-15 20:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue (obj, key, value) {
+    obj[key] = value
+  return obj
+  
+}
+
+function updateObjectWithKeyAndValue (obj, key, value) {
     var newObj = Object.assign(obj, {key: value})
   return newObj
   
 }
 
-function updateObjectWithKeyAndValue (obj, key, value) {
-    obj[key] = value
-  return obj
-  
-}
